36da0790a2cbdaf390fa019ba506d4d0fab03b13
Chris Rabl <rabl.christopher@gmail.com>
2021-06-27 14:53:33 -0600

    chore: add code challenge files to repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: '/leads',
+  pathMatch: 'full'
+}, {
+  path: 'leads',
+  loadChildren: () => import('./leads/leads-routing.module').then(m => m.LeadsRoutingModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
